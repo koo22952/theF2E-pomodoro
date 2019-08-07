727036b23cec5cb4e75eab6aff5f987b17aafaa2
Edward <tgfc057@tgfc.tw>
2019-08-07 16:38:34 +0800

    fast

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <div id="app">
+        <Home></Home>
+        <!--        <router-view/>-->
+    </div>
 </template>
 
+<script>
 
+import Home from './views/Home'
+
+export default {
+    components: {
+        Home
+    }
+
+}
+</script>
 <style lang="scss">
-  @import "./assets/all.scss";
-  #app {
-    width: 100%;
-    background-color: rgba(2, 3, 4, 0.1);
-    // background: linear-gradient(
-    //   to right,
-    //   #ffedf7,
-    //   #ffedf7 50%,
-    //   #003164 75%,
-    //   #003164 100%
-    // );
-  }
+    @import "./assets/all.scss";
+
+    #app {
+        width: 100%;
+        background-color: rgba(2, 3, 4, 0.1);
+        /*background: linear-gradient(*/
+        /*                to right,*/
+        /*                #ffedf7,*/
+        /*                #ffedf7 50%,*/
+        /*                #003164 75%,*/
+        /*                #003164 100%*/
+        /*);*/
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,119 +1,151 @@
 <template>
-  <div class="home">
-    <article class="home-left" :class="{'breakTime':isbreakTime}">
-      <AddInput :isbreakTime="isbreakTime" @onAddList="onAddList"></AddInput>
-      <Time @breckTime="(val)=>{isbreakTime = val}" :todoNow="todo"></Time>
-      <ListGroup :isbreakTime="isbreakTime" :todoList="todoList" @changeTodo="changeTodo"></ListGroup>
-    </article>
-    <aside class="home-right">
-      <nav class="home-right__nav">
-        <div class="nav-icon">
-          <!-- <i class="material-icons">list</i>
-          <i class="material-icons">assessment</i>
-          <i class="material-icons">library_music</i>-->
-        </div>
-      </nav>
-      <div class="nav-title">POMODORO</div>
-    </aside>
-  </div>
+    <div class="home">
+        <article class="home-left" :class="{'breakTime':isbreakTime}">
+            <AddInput :isbreakTime="isbreakTime" @onAddList="onAddList"></AddInput>
+            <Time @breckTime="(val)=>{isbreakTime = val}" :todoNow="todo"></Time>
+            <ListGroup :isbreakTime="isbreakTime" :todoList="todoList" @changeTodo="changeTodo" @onDelTodo="onDelTodo"></ListGroup>
+        </article>
+        <aside class="home-right">
+            <nav class="home-right__nav">
+                <div class="nav-icon">
+                    <!-- <i class="material-icons">list</i>
+                    <i class="material-icons">assessment</i>
+                    <i class="material-icons">library_music</i>-->
+                </div>
+            </nav>
+            <div class="nav-title">POMODORO</div>
+        </aside>
+        <b-modal ref="my-modal" hide-footer title="Using Component Methods">
+            <div class="d-block text-center">
+                <h3>Hello From My Modal!</h3>
+            </div>
+            <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
+            <b-button class="mt-2" variant="outline-danger" block @click="toggleModal">Toggle Me</b-button>
+        </b-modal>
+    </div>
 </template>
 
 <script>
-  import ListGroup from '../components/ListGroup'
-  import Time from '../components/Time'
-  import AddInput from '../components/AddInput'
-  export default {
+import ListGroup from '../components/ListGroup'
+import Time from '../components/Time'
+import AddInput from '../components/AddInput'
+
+export default {
     name: 'home',
     components: {
-      ListGroup,
-      AddInput,
-      Time,
+        ListGroup,
+        AddInput,
+        Time
     },
-    data() {
-      return {
-        isbreakTime: false,
-        todoList: [],
-        todo: ''
-      }
+    data () {
+        return {
+            isbreakTime: false,
+            todoList: [],
+            todo: '',
+            delItem: ''
+        }
     },
 
     methods: {
-      onAddList(value) {
-        this.todoList.push({
-          name: value,
-          status: false,
-          time: 15,
-        })
-      },
-      changeTodo(value) {
-        this.todo = value
-      }
-    },
-  }
+        onAddList (value) {
+            this.todoList.push({
+                name: value,
+                status: false,
+                time: 15
+            })
+        },
+        changeTodo (value) {
+            this.todo = value
+        },
+        onDelTodo (value) {
+            this.delItem = value
+            this.$refs['my-modal'].show()
+
+        },
+
+        hideModal () {
+            this.$refs['my-modal'].hide()
+        },
+        toggleModal () {
+            this.$refs['my-modal'].toggle('#toggle-btn')
+            let num = this.todoList.map(item => {
+                return item.name
+            }).indexOf(this.delItem.name)
+
+            this.todoList.splice(num, 1)
+        }
+    }
+}
 </script>
 
 
 <style lang="scss" scoped>
-  $lightColor: #ff4384;
-  $lightBg: #ffedf7;
-  $darkColor: #00a7ff;
-  $darkBg: #e5f3ff;
+    $lightColor: #ff4384;
+    $lightBg: #ffedf7;
+    $darkColor: #00a7ff;
+    $darkBg: #e5f3ff;
 
-  .home {
-    max-width: 1280px;
-    height: 100%;
-    display: flex;
-    margin: 0 auto;
-
-    &-left {
-      width: 65%;
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      background-color: $lightBg;
-      padding: 48px 0px 48px 85px;
-      position: relative;
-    }
-    &-right {
-      width: 35%;
-      height: 100%;
-      padding: 48px 85px 48px 0px;
-      color: white;
-      background-color: #003164;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      align-items: flex-end;
-      &__nav {
+    .home {
+        max-width: 1280px;
+        height: 100%;
         display: flex;
-        flex-wrap: wrap;
-        width: 36px;
-        .nav-icon {
-          i {
-            margin-bottom: 48px;
-            font-size: 36px;
-            cursor: pointer;
-          }
-        }
-      }
-      .nav-title {
-        font-size: 24px;
-        font-weight: bolder;
-        writing-mode: vertical-lr;
-      }
-    }
-  }
+        margin: 0 auto;
 
-  // 休息時間
-  .breakTime {
-    background-color: $darkBg;
-    .home-left__add {
-      color: $darkColor;
-      ::placeholder {
-        color: $darkColor;
-      }
+        &-left {
+            width: 65%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            background-color: $lightBg;
+            padding: 48px 0px 48px 85px;
+            position: relative;
+        }
+
+        &-right {
+            width: 35%;
+            height: 100%;
+            padding: 48px 85px 48px 0px;
+            color: white;
+            background-color: #003164;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: flex-end;
+
+            &__nav {
+                display: flex;
+                flex-wrap: wrap;
+                width: 36px;
+
+                .nav-icon {
+                    i {
+                        margin-bottom: 48px;
+                        font-size: 36px;
+                        cursor: pointer;
+                    }
+                }
+            }
+
+            .nav-title {
+                font-size: 24px;
+                font-weight: bolder;
+                writing-mode: vertical-lr;
+            }
+        }
     }
-  }
+
+    // 休息時間
+    .breakTime {
+        background-color: $darkBg;
+
+        .home-left__add {
+            color: $darkColor;
+
+            ::placeholder {
+                color: $darkColor;
+            }
+        }
+    }
 </style>
 
